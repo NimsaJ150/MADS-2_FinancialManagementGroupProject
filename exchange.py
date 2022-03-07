9042c4a6aa2a59ed11a28c78b5173b54da2895ec
Tajda Urankar <tajda.urankar@gmail.com>
2022-03-07 10:22:07 +0100

    additional features - days working and days as CEO

--- exchange.py
+++ exchange.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
-import openpyxl
 from sklearn.linear_model import LinearRegression
 
 """
@@ -13,19 +12,19 @@ ceo_data_raw = pd.read_csv("data/Execucomp_2006_-_2021_MTL.csv")
 company_data_raw = pd.read_csv("data/CCM_Fundamentals_Annual_2006_-_2021_new.csv")
 price_data_raw = pd.read_csv("data/return_annual.csv")
 
-company_data_raw.head()
-ceo_data_raw.head()
-
 company_data_raw_columns = company_data_raw.columns
 company_cols = ['GVKEY', 'LPERMNO', 'prcc_f', 'fyear']
 ceo_data_raw_columns = ceo_data_raw.columns
-ceo_cols = ['GVKEY', 'CO_PER_ROL', 'YEAR', 'AGE', 'BECAMECEO', 'TITLE', 'PCEO', 'LEFTOFC']
+ceo_cols = ['GVKEY', 'CO_PER_ROL', 'YEAR', 'AGE', 'TITLE', 'PCEO', 'BECAMECEO', 'LEFTOFC', 'LEFTCO', 'JOINED_CO']
 price_data_raw_columns = price_data_raw.columns
 price_cols = []
 
 # filter data
 company_data = company_data_raw[company_cols]
-company_data['fyear'] = company_data['fyear'].astype(int)
+company_data['fyear'] = company_data['fyear'].astype(int)   #  SettingWithCopyWarning: 
+# A value is trying to be set on a copy of a slice from a DataFrame. 
+# Try using .loc[row_indexer,col_indexer] = value instead
+
 
 ceo_data = ceo_data_raw[ceo_cols]
 ceo_data = ceo_data[ceo_data.PCEO == "CEO"]
@@ -46,7 +45,7 @@ data_joined = data_joined.join(ceo_data.set_index(['GVKEY', 'YEAR']), on=['GVKEY
 Preprocess data
 """
 
-# drop nan in prcc_f or ajex/ajp columns
+# drop nan in prcc_f or ajex/ajp columns --> this is deleting everything except 6 rows??
 data_joined.dropna(inplace=True)
 
 data_joined.apply(lambda x: x.astype(str).str.lower())
@@ -71,27 +70,50 @@ data_joined['dummy_chairman_president'] = data_joined['TITLE'].str.contains('|'.
 # drop columns only important for joining
 data_joined.drop(['GVKEY', 'CO_PER_ROL', 'PCEO', 'TITLE'], axis=1, inplace=True) #ajex, ajp removed
 
+
 """
 Additional features
 """
 # 3 year requirement for managers
 # BECAMECEO LEFTOFC
-data_joined['BECAMECEO'] = pd.to_datetime(data_joined['BECAMECEO'], format='%Y%m%d')
-data_joined['LEFTOFC'] = pd.to_datetime(data_joined['LEFTOFC'], format='%Y%m%d')
+# data_joined['BECAMECEO'] = pd.to_datetime(data_joined['BECAMECEO'], format='%Y%m%d')
+# data_joined['LEFTOFC'] = pd.to_datetime(data_joined['LEFTOFC'], format='%Y%m%d')
 
-data_joined['3Y_THRESH'] = (data_joined['LEFTOFC']-data_joined['BECAMECEO'])/365.35 #incomplete
+# data_joined['3Y_THRESH'] = (data_joined['LEFTOFC']-data_joined['BECAMECEO'])/365.35 #incomplete
 # data_joined['3Y_THRESH'] >= 3
 
 
+# print(data_joined["JOINED_CO"].isnull().sum())
+# print(data_joined["JOINED_CO"])
+
 # how many years as CEO - ceo_tenure
-# how many years working there -
+# PROBLEM - NULL VALUES, negative values?? :)
+temp = pd.DataFrame(data_joined, columns=['BECAMECEO', 'LEFTOFC', 'LEFTCO', 'JOINED_CO'])
+temp['LEFTOFC'] = pd.to_datetime(temp['LEFTOFC'], format='%Y%m%d')
+temp['BECAMECEO'] = pd.to_datetime(temp['BECAMECEO'], format='%Y%m%d')
+temp['LEFTCO'] = pd.to_datetime(temp['LEFTCO'], format='%Y%m%d')
+temp['JOINED_CO'] = pd.to_datetime(temp['JOINED_CO'], format='%Y%m%d')
+
+data_joined['ceo_tenure'] = (temp['LEFTOFC'] - temp['BECAMECEO']).dt.days
+# print(data_joined['ceo_tenure'])
+
+# how many years working there 
+# PROBLEM - NULL VALUES
+data_joined['working_days'] = (temp['LEFTCO'] - temp['JOINED_CO']).dt.days
+# print(data_joined['working_days'])
+
 # percentage change in stock prices
 
 # -> group for single CEOs - avg_change_in_sp
 
+# drop columns only important for calculating date related attributes
+data_joined.drop(['JOINED_CO', 'BECAMECEO', 'LEFTCO', 'LEFTOFC'], axis=1, inplace=True)
+
+
 """
 Fixed effects
 """
+
 
 """
 Linear Regression
