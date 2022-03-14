d41357e4f16e5096077c6be18ca3df94448d010e
MyThuL <92041179+MyThuL@users.noreply.github.com>
2022-03-14 16:13:26 +0100

    Update compustat_annual_return.py

--- compustat_annual_return.py
+++ compustat_annual_return.py
@@ -8,14 +8,14 @@ Load data
 """
 
 # import data
-ccm = pd.read_csv("data/CCM_Fundamentals_Annual_2006_-_2021_new.csv")
+ccm = pd.read_csv("/Users/mythulam/Desktop/Masters/02_Spring_2022/03_Guided_Studies_in_Financial_Management/Group_Project/Data/Compustat/Compustat_PRCCF_AJEX.csv")
 # show data
 ccm.head()
+ccm['adjust_prccf'] = ccm['prcc_f']*ccm['ajex']
+ccm_return = ccm[['LPERMNO', 'fyear', 'prcc_f', 'ajex', 'adjust_prccf']]
 
-ccm_return = ccm[['LPERMNO', 'fyear', 'prcc_f']]
-
-test = ccm_return.groupby(by=['LPERMNO'])['prcc_f'].pct_change()
+test = ccm_return.groupby(by=['LPERMNO'])['adjust_prccf'].pct_change().where(ccm_return.groupby(['LPERMNO'])['fyear'].diff()==1)
 ccm_return_csv = pd.concat([ccm_return, test], axis=1).reset_index()
 ccm_return_csv.head()
-ccm_return_csv.columns = ['index', 'LPERMNO', 'fyear', 'prcc_f', 'prcc_pcchg']
-ccm_return_csv.to_csv(path_or_buf='/Users/carlasuzanneweidner/Downloads/compustat_annual_return.csv', index=False)
+ccm_return_csv.columns = ['index', 'LPERMNO', 'fyear', 'prcc_f', 'ajex', 'adjust_prccf', 'prcc_pcchg']
+ccm_return_csv.to_csv(path_or_buf='/Users/mythulam/Desktop/Masters/02_Spring_2022/03_Guided_Studies_in_Financial_Management/Group_Project/Data/Compustat/Compustat_annual_return_NEW.csv', index=False)
